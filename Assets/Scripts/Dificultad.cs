6a068687ca107c6ec3ee180ec953f79bf7e716e0
N-Naith <naomynaithvargas@icloud.com>
2022-09-06 18:15:50 -0600

    firts commit

---
+++ Assets/Scripts/Dificultad.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dificultad : MonoBehaviour
+{
+    public string nombreDificultad;
+
+    public Pregunta[] preguntas;
+}
---
+++ Assets/Scripts/PanelComplementario.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelComplementario : MonoBehaviour
+{
+   public GameObject panelComplementario;
+
+   public Game game;
+
+   public void Continuar(){
+    panelComplementario.SetActive(false);
+    game.setPregunta();
+   }
+
+   public void desplegar(){
+    panelComplementario.SetActive(true);
+   }
+}
